91e5de167eacb74ee666c2ab5b10f57c5ff44007
rvatsdeo <rvcipher1@gmail.com>
2026-02-06 09:22:08 +0530

    fix port

--- main.py
+++ main.py
@@ -38,5 +38,6 @@ app.mount("/mcp", mcp.http_app())
 if __name__ == "__main__":
     import uvicorn
 
+    port = int(os.environ.get("PORT", 8080))
     # Run the FastAPI app directly
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=port)
